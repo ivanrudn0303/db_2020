acd6cc0caa1c07e80615817e20354c3d22eeaeec
Ivan Kozlov <ivanrudn0303@gmail.com>
2020-09-02 02:02:06 +0300

    Done HW2

--- src/main/java/homework/riddle_game/RiddleGame.java
+++ src/main/java/homework/riddle_game/RiddleGame.java
@@ -3,6 +3,7 @@ package homework.riddle_game;
 import design_patterns.template_method.Game;
 
 import javax.swing.*;
+import java.util.Random;
 
 /**
  * @author Evgeny Borisov
@@ -12,6 +13,7 @@ public class RiddleGame implements Game {
     //todo
 
     private final int maxLimit;
+    private int answer;
 
     public RiddleGame(int max) {
         this.maxLimit = max;
@@ -20,17 +22,34 @@ public class RiddleGame implements Game {
 
     public static void main(String[] args) {
 
-        int s = Integer.parseInt(JOptionPane.showInputDialog("input your number"));
-        System.out.println(s);
+        int maximum = Integer.parseInt(JOptionPane.showInputDialog("set number limits"));
+        RiddleGame riddleGame = new RiddleGame(maximum);
+        riddleGame.play();
     }
-
 
 
 
 
     @Override
     public void play() {
+        generateAnswer();
+        int guess;
+        int attempts = 0;
+        do {
+            attempts++;
+            guess = Integer.parseInt(JOptionPane.showInputDialog("Enter your guess"));
+        } while (!correctGuess(guess));
+        System.out.println("Good job. It took just " + attempts + " attempts");
+    }
 
+    private void generateAnswer() {
+        Random rn = new Random();
+        answer = rn.nextInt(this.maxLimit + 1);
+        System.out.println(answer);
+    }
+
+    private boolean correctGuess(int guess) {
+        return answer == guess;
     }
 }
 
